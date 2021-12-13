164860927760ebfd7614d9064d8f49f11db2daab
Michael Smyers <michael@smyers.net>
2021-12-12 20:05:07 -0800

    clean

--- src/sitemap.js
+++ src/sitemap.js
@@ -13,57 +13,6 @@ export default {
 
     PRODUCTS: {
 
-        // 'cit1x': {
-        //     fqdn: 'https://www.cit1x.finance',
-        //
-        //     name: 'Cit1x',
-        //     slogan: 'We help Citizens trade.',
-        //     description: "Efficient Trade Router",
-        //
-        //     page: {
-        //         href: '/cit1x',
-        //         meta: {},
-        //         hero: {
-        //             strings: ['Citizens 1st', 'Cit1'], suffix: 'x'
-        //         },
-        //         action: 'Efficient Trade Router'
-        //     },
-        // },
-        //
-        // 'cit1s': {
-        //     fqdn: 'https://www.cit1s.finance',
-        //
-        //     name: 'Cit1s',
-        //     slogan: 'We help Citizens swap.',
-        //     description: "Citizen Swap Shop",
-        //
-        //     page: {
-        //         href: '/cit1s',
-        //         meta: {},
-        //         hero: {
-        //             strings: ['Citizens 1st', 'Cit1'], suffix: 's'
-        //         },
-        //         action: 'Citizen Swap Shop'
-        //     },
-        // },
-        //
-        // 'uusd': {
-        //     fqdn: 'https://www.uusd.finance',
-        //
-        //     name: 'UUSD',
-        //     slogan: 'We help Citizens save.',
-        //     description: "Meta Stable Coin",
-        //
-        //     page: {
-        //         href: '/uusd',
-        //         meta: {},
-        //         hero: {
-        //             strings: ['Universal', 'UBI', 'U'], suffix: 'USD'
-        //         },
-        //         action: 'Meta Stable Coin'
-        //     },
-        // },
-
     },
 
     forPage(id) {
